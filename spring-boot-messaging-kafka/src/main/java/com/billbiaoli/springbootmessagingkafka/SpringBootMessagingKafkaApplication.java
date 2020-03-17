d3d8ee940a422616c0280ced22320a62aebffd69
MARS_Penguine <lbit123@outlook.com>
2020-03-18 10:19:54 +1100

    Initialize

---
+++ spring-boot-messaging-kafka/src/main/java/com/billbiaoli/springbootmessagingkafka/SpringBootMessagingKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.billbiaoli.springbootmessagingkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMessagingKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMessagingKafkaApplication.class, args);
+    }
+
+}
---
+++ spring-boot-messaging-kafka/src/test/java/com/billbiaoli/springbootmessagingkafka/SpringBootMessagingKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billbiaoli.springbootmessagingkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMessagingKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
