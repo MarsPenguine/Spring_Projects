f5da6117ca77539196a9bb87d7d015430d887df7
MARS_Penguine <lbit123@outlook.com>
2020-03-20 09:48:42 +1100

    Update for Embeded Mongo Config

--- spring-boot-reactive/src/main/java/com/biaolibill/springbootreactive/config/DataConfig.java
+++ spring-boot-reactive/src/main/java/com/biaolibill/springbootreactive/config/DataConfig.java
@@ -1,33 +1,27 @@
 package com.biaolibill.springbootreactive.config;
 
 import com.mongodb.reactivestreams.client.MongoClient;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
-import org.springframework.data.mongodb.core.ReactiveMongoOperations;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 /**
  * Mongo configuration
  */
 
-@Configuration
-@Import(EmbeddedMongoAutoConfiguration.class)
-public class DataConfig {
+@EnableReactiveMongoRepositories
+public class DataConfig extends AbstractReactiveMongoConfiguration {
 
     public static final String DATABASE_NAME = "reservations";
 
-    @Bean
-    public ReactiveMongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
-        return new SimpleReactiveMongoDatabaseFactory(mongoClient, DATABASE_NAME);
+    @Override
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create();
     }
 
-    @Bean
-    public ReactiveMongoOperations reactiveMongoTemplate(ReactiveMongoDatabaseFactory mongoDatabaseFactory) {
-        return new ReactiveMongoTemplate(mongoDatabaseFactory);
+    @Override
+    protected String getDatabaseName() {
+        return DATABASE_NAME;
     }
 }
 
